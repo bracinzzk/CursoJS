2f2d43e7e16f8f2f040263d01296f8ec40acc3cd
blessed0071 <126590810+bracinzzk@users.noreply.github.com>
2024-09-19 08:20:38 -0300

    Add files via upload

---
+++ Aula2.js
@@ -0,0 +1,12 @@
+console.log("Meu primeiro programa: trabalhando com variavel");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idad)
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+2;
+console.log(idadeSomad);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CursoJS
+# cursojava
